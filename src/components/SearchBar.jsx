937080eb29f3e4e8c113c1419e3888fd3af3ec69
anna0625 <anna860625@gmail.com>
2022-09-18 17:03:22 +1000

    change class to className in SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,31 +3,36 @@ import React from "react";
 export const SearchBar = () => {
   return (
     <>
-      <div class="my-6 flex rounded-md shadow-sm">
-        <div class="relative flex flex-grow items-stretch focus-within:z-10">
-          <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+      <div className="my-6 flex rounded-md shadow-sm">
+        <div className="relative flex flex-grow items-stretch focus-within:z-10">
+          <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
             {/* <!-- Heroicon name: mini/users --> */}
             <svg
-              class="h-5 w-5 text-gray-400"
               xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="#f5cac3"
-              aria-hidden="true"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="#f5cac3"
+              className="w-6 h-6"
             >
-              <path d="M7 8a3 3 0 100-6 3 3 0 000 6zM14.5 9a2.5 2.5 0 100-5 2.5 2.5 0 000 5zM1.615 16.428a1.224 1.224 0 01-.569-1.175 6.002 6.002 0 0111.908 0c.058.467-.172.92-.57 1.174A9.953 9.953 0 017 18a9.953 9.953 0 01-5.385-1.572zM14.5 16h-.106c.07-.297.088-.611.048-.933a7.47 7.47 0 00-1.588-3.755 4.502 4.502 0 015.874 2.636.818.818 0 01-.36.98A7.465 7.465 0 0114.5 16z" />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+              />
             </svg>
           </div>
           <input
             type="email"
             name="email"
             id="email"
-            class="block w-full rounded-none rounded-l-md focus:outline-none focus:ring-2 border-theme-3 pl-10 focus:border-theme-1 focus:ring-theme-1 sm:text-sm text-theme-4 placeholder:text-theme-4 bg-theme-2 shadow-sm"
+            className="block w-full rounded-none rounded-l-md focus:outline-none focus:ring-2 border-theme-3 pl-10 focus:border-theme-1 focus:ring-theme-1 sm:text-sm text-theme-4 placeholder:text-theme-4 bg-theme-2 shadow-sm"
             placeholder="John Smith"
           />
         </div>
         <button
           type="button"
-          class="relative -ml-px inline-flex items-center space-x-2 rounded-r-md border border-theme-4 bg-theme-3/50 px-4 py-2 text-sm font-medium text-theme-4 hover:bg-theme-2 focus:border-theme-4 focus:outline-none focus:ring-2 focus:ring-theme-4 shadow-sm"
+          className="relative -ml-px inline-flex items-center space-x-2 rounded-r-md border border-theme-4 bg-theme-3/50 px-4 py-2 text-sm font-medium text-theme-4 hover:bg-theme-2 focus:border-theme-4 focus:outline-none focus:ring-2 focus:ring-theme-4 shadow-sm"
         >
           {/* <!-- Heroicon name: mini/bars-arrow-up --> */}
           <svg
